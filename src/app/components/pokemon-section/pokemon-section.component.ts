b1854f3aab982485e6554b778e0bec08feecc648
PedroAloonso <pedro.alonsopa@hotmail.com>
2025-04-03 09:54:13 -0300

    modifing pokemon-section to use signals

--- src/app/components/pokemon-section/pokemon-section.component.ts
+++ src/app/components/pokemon-section/pokemon-section.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, effect, OnInit, signal } from '@angular/core';
 import { Pokemon } from '../../interfaces/pokemon';
 import { PokemonService } from '../../services/pokemon/pokemon.service';
 
@@ -9,37 +9,33 @@ import { PokemonService } from '../../services/pokemon/pokemon.service';
 	templateUrl: './pokemon-section.component.html',
 	styleUrl: './pokemon-section.component.scss',
 })
-export class PokemonSectionComponent implements OnInit {
-	pokemonlist!: Pokemon[];
-	prevPage!: number;
-	nextPage!: number;
+export class PokemonSectionComponent {
+	pokemonList = signal<Pokemon[]>([]);
+	currentPage = signal<number>(1);
+	prevPage = signal<number | null>(null);
+	nextPage = signal<number | null>(null);
 
-	constructor(private pokemonService: PokemonService) {}
-
-	ngOnInit(): void {
-		this.pokemonService.getPokemonList().subscribe((value) => {
-			console.log(value);
-			this.pokemonlist = value.results;
-			this.prevPage = value.prev_offset;
-			this.nextPage = value.next_offset;
+	constructor(private pokemonService: PokemonService) {
+		effect(() => {
+			this.loadPokemonList((this.currentPage() - 1) * 20);
 		});
 	}
 
-	goToNextPage(offset: number) {
+	private loadPokemonList(offset: number) {
 		this.pokemonService.getPokemonList(offset).subscribe((value) => {
-			console.log(value);
-			this.pokemonlist = value.results;
-			this.prevPage = value.prev_offset;
-			this.nextPage = value.next_offset;
+			this.pokemonList.set(value.results);
+			this.prevPage.set(value.prev_offset);
+			this.nextPage.set(value.next_offset);
 		});
 	}
 
-	goToPreviusPage(offset: number) {
-		this.pokemonService.getPokemonList(offset).subscribe((value) => {
-			console.log(value);
-			this.pokemonlist = value.results;
-			this.prevPage = value.prev_offset;
-			this.nextPage = value.next_offset;
-		});
+	goToNextPage() {
+		this.currentPage.update((prevValue) => prevValue + 1);
+	}
+
+	goToPreviusPage() {
+		if (this.prevPage() !== null) {
+			this.currentPage.set(this.currentPage() - 1);
+		}
 	}
 }
